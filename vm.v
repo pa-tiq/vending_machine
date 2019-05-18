750d0cc2bab9eca476f4c5f23e09d959ec8e5f09
Patrick <patrickdsilva99@gmail.com>
2019-05-18 12:03:13 -0300

    algumas variáveis

--- vm.v
+++ vm.v
@@ -1,9 +1,27 @@
 module vm(x , y, clock,reset);
-	input reais,clock,reset;
+
+	input clock,reset;
+	input [1:0] SW1,SW2,SW3,SW4,SW5;
+	//SW1 = R$0,05
+	//SW2 = R$0,10
+	//SW3 = R$0,25
+	//SW4 = R$0,50
+	//SW5 = R$1,00
+	
+	input [1:0] SW9;
+	//SW9 = Give up
+	
+	input [1:0] KEY0, KEY1;
+	//KEY0 = Change selection
+	//KEY1 = Select product
+	
+	output [0:6] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
 	output reg y;
 	
+	reg [7:0] money,change;
+	
 	reg [1:0] state, previous_state; 
-	parameter start=0,select=1,change=2,
+	parameter start=0,select=1,give_change=2,
 			chocolate = 1.20;
 			coffee = 1.00;
 			juice = 0.70;
