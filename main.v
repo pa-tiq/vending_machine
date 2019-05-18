c33abc3bbc28f52f99e2d4deb4407bbdc47da539
Patrick <patrickdsilva99@gmail.com>
2019-05-18 16:56:14 -0300

    criação das variáveis para os LEDs de cada SW, começo da implementação das contas com o dinheiro, inclusão da função para mostrar o dinheiro no display de 7 segmentos.

--- main.v
+++ main.v
@@ -6,17 +6,19 @@ module tb;
 	reg clock, reset;
 	source source(.x(x));
 	vm vm(.x(x), .y(y), .clock(clock), .reset(reset));
+	
 	initial begin
-	$dumpfile("max10.vcd");  //especifica em qual arquivo serao armazenadas as formas de onda
-	$dumpvars;  //especifica para colocar todos os sinais no add.vcd 
-	reset=0;
-	clock = 0;
-	#5;
+		$dumpfile("vm.vcd");  //especifica em qual arquivo serao armazenadas as formas de onda
+		$dumpvars;  //especifica para colocar todos os sinais no add.vcd 
+		reset=0;
+		clock = 0;
+		#5;
 
-	reset=1;
-	#5;
-	#150 $finish;
+		reset=1;
+		#5;
+		#150 $finish;
 	end
+	
 	always
 	 # 5 clock = ~clock;
 endmodule
--- vm.v
+++ vm.v
@@ -1,6 +1,9 @@
 module vm(x , y, clock,reset);
 
 	input clock,reset;
+	
+	input [1:0] LEDR1, LEDR2, LEDR3, LEDR4, LEDR5
+	//Leds (SW1 to SW5)
 	input [1:0] SW1,SW2,SW3,SW4,SW5;
 	//SW1 = R$0,05
 	//SW2 = R$0,10
@@ -21,28 +24,51 @@ module vm(x , y, clock,reset);
 	reg [7:0] money,change;
 	
 	reg [1:0] state, previous_state; 
-	parameter start=0,select=1,give_change=2,
+	parameter select=0,insert_money=1,give_change=2,
 			chocolate = 1.20;
 			coffee = 1.00;
 			juice = 0.70;
 	
-	//parte sequencial
 	always @(posedge clock, negedge reset) begin
 		if (~reset) begin
-		state<=start;
-		previous_state<=start;
-		y<=0;
+		state<=select;
+		previous_state<=select;
+		money7seg digit(0, HEX0,HEX1,HEX2);
 		end
 		else begin
 			case (state)
-				inicio: begin
-					if (x==1) state<=s1;
-					y<=0;
-				end
-				s1: begin
+				select: begin
 					if (x==1) state<=s11;
 					else state<=inicio;
 					y<=0;
+				end
+				insert_money: begin
+					if (SW1==1) begin
+						LEDR1 = 1;
+						money += 5;
+						money7seg digit(money, HEX0,HEX1,HEX2);
+						LEDR1 = 0;						
+					end
+					if (SW2==1) begin
+						LEDR2 = 1;
+						money += 10;
+						LEDR2 = 0;						
+					end
+					if (SW3==1) begin
+						LEDR3 = 1;
+						money += 25;
+						LEDR3 = 0;						
+					end
+					if (SW4==1) begin
+						LEDR4 = 1;
+						money += 50;
+						LEDR4 = 0;						
+					end
+					if (SW5==1) begin
+						LEDR5 = 1;
+						money += 100;
+						LEDR5 = 0;						
+					end
 				end
 				s11: begin
 					if(x==0) state<=s110;
@@ -61,7 +87,48 @@ module vm(x , y, clock,reset);
 			endcase
 		previous_state<=state;
 		end
-	end
-  
+	end  
  
+endmodule
+
+module money7seg(value,ds0,ds1,ds2);
+
+	
+	
+	bcd7seg digit2 (ds0, HEX2);
+	bcd7seg digit1 (ds1, HEX1);
+	bcd7seg digit0 (ds2, HEX0);	
+endmodule
+
+module bcd7seg (bcd, display);
+	input [3:0] bcd;
+	output [0:6] display;
+	reg [0:6] display;
+	/*
+	 *       0  
+	 *      ---  
+	 *     |   |
+	 *    5|   |1
+	 *     | 6 |
+	 *      ---  
+	 *     |   |
+	 *    4|   |2
+	 *     |   |
+	 *      ---  
+	 *       3  
+	 */
+	always @ (bcd)
+		case (bcd)
+			4'h0: display = 7'b0000001;
+			4'h1: display = 7'b1001111;
+			4'h2: display = 7'b0010010;
+			4'h3: display = 7'b0000110;
+			4'h4: display = 7'b1001100;
+			4'h5: display = 7'b0100100;
+			4'h6: display = 7'b1100000;
+			4'h7: display = 7'b0001111;
+			4'h8: display = 7'b0000000;
+			4'h9: display = 7'b0001100;
+			default: display = 7'b1111111;
+		endcase
 endmodule
